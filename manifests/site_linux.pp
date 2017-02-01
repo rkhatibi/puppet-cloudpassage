27b294a7286e7c3b66d46b71bf0a894308d3bc62
Jye Lee <jlee@cloudpassage.com>
2017-02-01 09:22:00 -0800

    remove hardcoded agent keys

--- manifests/site_linux.pp
+++ manifests/site_linux.pp
@@ -1,6 +1,6 @@
 node default {
         class {'cloudpassage':
-                agent_key => '15efc6fd864ce8471a71c5aa1000e06f',
+                agent_key => $halo_agent_key,
                 tag => 'hlee'
         }
 }
