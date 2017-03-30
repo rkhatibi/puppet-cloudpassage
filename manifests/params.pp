b4fbbb1d4a62955dac78fb17270bf086a1920050
Jye Lee <jlee@cloudpassage.com>
2017-03-30 09:01:17 -0700

    set shared managed_repo so windows dont fail on unknown var

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,8 @@ class cloudpassage::params(
   Undef $server_label     = undef,
   Boolean $service_enable = true,
   Boolean $service_ensure = true,
-  Undef $tag              = undef
+  Undef $tag              = undef,
+  Boolean $manage_repos   = true
 ) { if $facts['kernel'] == 'windows' {
       $destination_dir = 'c:/tmp'
       $package_file    = 'cphalo-3.9.7-win64.exe'
@@ -19,7 +20,6 @@ class cloudpassage::params(
       $package_url     = "https://production.packages.cloudpassage.com/windows/${package_file}"
       $service_name    = 'cphalo'
   } else {
-      $manage_repos = true
       $package_name = 'cphalo'
       $service_name = 'cphalod'
       $repo_ensure  = 'present'
