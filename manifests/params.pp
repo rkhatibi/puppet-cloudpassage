f6d8ebfffe23a6f26239cc3c9059e419920d8fe8
Jye Lee <jlee@cloudpassage.com>
2017-03-30 12:31:50 -0700

    leave only shared vars in if statement

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,16 +12,16 @@ class cloudpassage::params(
   Boolean $service_enable = true,
   Boolean $service_ensure = true,
   Undef $tag              = undef,
-  Boolean $manage_repos   = true
+  Boolean $manage_repos   = true,
+  String $destination_dir = 'c:/tmp',
+  String $package_file    = 'cphalo-3.9.7-win64.exe',
+  String $package_url     = "https://production.packages.cloudpassage.com/windows/${package_file}",
+  String $repo_ensure     = 'present'
 ) { if $facts['kernel'] == 'windows' {
-      $destination_dir = 'c:/tmp'
-      $package_file    = 'cphalo-3.9.7-win64.exe'
       $package_name    = 'CloudPassage Halo'
-      $package_url     = "https://production.packages.cloudpassage.com/windows/${package_file}"
       $service_name    = 'cphalo'
   } else {
-      $package_name = 'cphalo'
-      $service_name = 'cphalod'
-      $repo_ensure  = 'present'
+      $package_name    = 'cphalo'
+      $service_name    = 'cphalod'
   }
 }
