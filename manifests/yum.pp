f007ce7c52e39ef9ee8b5f73201674d3c1eb5e81
Hana Lee <hlee@cloudpassage.com>
2017-12-06 11:09:32 -0800

    change yum.pp to match the new puppet syntax

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -1,7 +1,7 @@
 # yum repo configurations
 class cloudpassage::yum {
   yumrepo { 'cloudpassage':
-    baseurl  => "http://packages.cloudpassage.com/redhat/${::basearch}",
+    baseurl  => "https://production.packages.cloudpassage.com/redhat/\$basearch",
     descr    => 'CloudPassage',
     gpgcheck => 1,
     gpgkey   => 'https://production.packages.cloudpassage.com/cloudpassage.packages.key'
